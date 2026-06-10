36bd846b816eaf39a0380b5bc44825c477f3577e
chanCo <loinsective@naver.com>
2026-06-10 16:56:16 +0900

    ADD: 콜라츠 수열 만들기

---
+++ lv.0/콜라츠 수열 만들기.js
@@ -0,0 +1,20 @@
+function solution(n) {
+  let result = [];
+
+  while (true) {
+    result.push(n);
+    if (n === 1) {
+      break;
+    }
+
+    if (n % 2 === 0) {
+      n = n / 2;
+    } else {
+      n = 3 * n + 1;
+    }
+  }
+
+  return result;
+}
+
+solution(10);
