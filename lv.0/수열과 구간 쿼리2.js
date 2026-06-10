ba95a50af65807cf86e4364d6768d8f094981b2f
chanCo <loinsective@naver.com>
2026-06-10 15:00:47 +0900

    ADD: 수열과 구간 쿼리2

---
+++ lv.0/수열과 구간 쿼리2.js
@@ -0,0 +1,23 @@
+function solution(arr, queries) {
+  let result = [];
+
+  queries.map((query, queryIndex) => {
+    const sliced = arr.slice(query[0], query[1] + 1);
+
+    const number = sliced.filter((number) => number > query[2]);
+    const minNumber = number.length ? Math.min(...number) : -1;
+
+    result.push(minNumber);
+  });
+
+  return result;
+}
+
+solution(
+  [0, 1, 2, 4, 3],
+  [
+    [0, 4, 2],
+    [0, 3, 2],
+    [0, 2, 2],
+  ],
+);
