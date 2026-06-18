700f0f48aea69f7a20af731e67e8d89a84f4e7ce
chanCo <loinsective@naver.com>
2026-06-18 11:12:28 +0900

    ADD: 최대공약수와 최소공배수

---
+++ lv.1/최대공약수와 최소공배수.js
@@ -0,0 +1,12 @@
+function solution(n, m) {
+  let gcd = 0;
+
+  for (let i = 1; i <= Math.min(n, m); i++) {
+    if (n % i === 0 && m % i === 0) gcd = i;
+  }
+
+  return [gcd, (n * m) / gcd]
+}
+
+solution(3, 12);
+solution(2, 5);
